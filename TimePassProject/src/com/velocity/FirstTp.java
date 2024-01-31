5c6c876b48fb76a1d476811256f6710975915565
praful <prafultells@gmail.com>
2024-01-31 19:15:24 +0530

    added sorting number

---
+++ TimePassProject/src/com/velocity/FirstTp.java
@@ -0,0 +1,16 @@
+package com.velocity;
+import java.util.*;
+
+public class FirstTp {
+
+	public static void main(String[] args) {
+		
+		Set<Integer> s = new TreeSet<Integer>();
+		s.add(30);
+		s.add(20);
+		s.add(50);
+		s.add(10);
+		
+		System.out.println(s);
+	}
+}
